7ea4225996eeed9911be4bd41ff73ff203d63511
Patrick-Ngige <wakemanjajr@gmail.com>
2024-07-23 23:46:13 +0300

    final phase

--- scripts.js
+++ scripts.js
@@ -2,8 +2,24 @@ const body = document.querySelector('body'),
     hourHand = document.querySelector('.hour'),
     minHand = document.querySelector('.minute'),
     secHand = document.querySelector('.second'),
-    modewitch = document.querySelector('.mode-switch');
+    modeSwitch = document.querySelector('.mode-switch');
 
+
+
+    //event listener for mode-switch button
+    modeSwitch.addEventListener("click", () => {
+        body.classList.toggle("dark");
+        if (body.style.backgroundColor !== 'black') {
+             body.style.backgroundColor = 'black';
+             modeSwitch.textContent = "Light Mode";
+        } else {
+            body.style.backgroundColor = 'white';
+            modeSwitch.textContent = "Dark Mode";
+
+}
+
+        // body.classList.toggle("dark");
+    })
 
     const updateTime = () => {
         
@@ -22,4 +38,8 @@ const body = document.querySelector('body'),
         hourHand.style.transform = `rotate(${hourToDeg}deg)`
     }
 
+    //set clock hands every second
     setInterval(updateTime, 1000);
+
+    //call updateTime function on page load
+    updateTime();
